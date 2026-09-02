1dd270832ad2cc8764427c89bf24f5b7fba62c55
Egorzhmurov <zhmurovegor99@gmail.com>
2026-09-02 21:49:28 +0300

    Update frontend user table and components

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from './getuser/user';
+
+// Main App component
+function App() {
+  return (
+    <div>
+      <User />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/getuser/user.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './user.css';
+
+// User component to display the user table interface
+function User() {
+  return (
+    <div className='userTable'>
+      <h3>User Table</h3>
+    </div>
+  );
+}
+
+export default User;
