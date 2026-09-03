da0361bd5c8a4434c2201b34d3423e6469c96eac
Egorzhmurov <zhmurovegor99@gmail.com>
2026-09-03 15:53:53 +0300

    Update frontend user table and components add style to user table

--- client/src/getuser/user.jsx
+++ client/src/getuser/user.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import './user.css';
+import './user.css'; // Importing component styles
 
-// User component to display the user table interface
-function User() {
+const User = () => {
   return (
-    <div className='userTable'>
-      <h3>User Table</h3>
+    /* Card container for the user table */
+    <div className="userTable">
+      {/* Primary Bootstrap button to add a user */}
+      <button type="button" className="btn btn-primary">Add User</button>
+
+      {/* Main Bootstrap bordered table */}
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">S.No.</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Address</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>John</td>
+            <td>john@gmail.com</td>
+            <td>Canada</td>
+            <td>Update | Delete</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default User;
